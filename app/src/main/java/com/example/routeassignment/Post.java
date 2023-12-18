2f6bb8b64c612f21a1ef69ec508bca76d89c8f4f
Mohamed3fify <ihazem461@gmail.com>
2023-12-18 22:03:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/routeassignment/Post.java
@@ -0,0 +1,13 @@
+package com.example.routeassignment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Post extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
